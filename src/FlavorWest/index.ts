e65c332d6d2d35881ff7bb29333d47c7a6e8bed5
brendan-c <brendan.k.cooney@gmail.com>
2020-06-11 19:37:34 -0400

    Added FlavorWest

---
+++ src/FlavorWest/index.ts
@@ -0,0 +1,25 @@
+import fetch from 'node-fetch'
+import { FlavourInterface } from '../types'
+const cheerioModule = require('cheerio')
+
+export default async function getFlavorWest() {
+  const res = await fetch(
+    'https://www.flavorwest.com/water-soluble-flavoring.html?product_list_limit=all'
+  )
+  const $ = cheerioModule.load(await res.text())
+
+  const flavors: FlavourInterface[] = []
+
+  $('.product-item-link').each(function () {
+    const flavor = $(this)
+    flavors.push({
+      name: flavor
+        .text()
+        .trim()
+        .replace(/(\r\n|\n|\r)/gm, ''),
+      manufacturer: 'FW',
+    })
+  })
+  flavors.shift()
+  return flavors
+}
